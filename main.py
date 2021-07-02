1962e3244d6cb6511f8af2fb0e477252eb6d73ea
Laurent <laurent.kl1996@gmail.com>
2021-07-02 04:05:11 +0200

    MeteoHour 0.2

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import discord
+from datetime import datetime, timedelta
 from discord.ext import commands
 import requests
 from bs4 import BeautifulSoup
@@ -8,12 +9,29 @@ import json
 
 bot = commands.Bot(command_prefix='.')
 
+bot.remove_command('help')
+
+
+@bot.command()
+async def help(ctx):
+    embed = discord.Embed(title="Menu d'aide", url="https://google.com",description="Ceci est le menu d'aide", color=0XFF5733)
+    embed.add_field(name=".ping", value='Pour savoir si le bot est On', inline=False)
+    embed.add_field(name=".Clear <nombre_messages>", value='Supprime un certain nombre de messages dans le channel', inline=False)
+    embed.add_field(name=".MeteoNow <ville>", value="Donne la météo à l'instant présent dans une ville choisie", inline=False)
+    embed.add_field(name=".MeteoHour <ville>", value="Donne la météo pour toute la journée d'une ville choisie", inline=False)
+    await ctx.send(embed=embed)
+
 @bot.command()
 async def ping(ctx):
     await ctx.send('pong')
-    
-@bot.command()
-async def Meteo(ctx, search: str):
+
+@bot.command(name = 'Clear', help="Cette fonction supprime 5 messages mais peut en supprimer jusqu'a 100: .Clear <nombre_de_messages>")
+async def Clear(ctx, amount = 5):
+    await ctx.channel.purge(limit = amount)
+
+
+@bot.command(name = 'MeteoNow', help="Affiche la température a l'instant présent : \n.MeteoNow <ville>")
+async def MeteoNow(ctx, search: str):
 
     def FindMeteo(): 
         apiKey = 'a440a00c0ff4abe85ed3319d7e208d73'
@@ -28,10 +46,6 @@ async def Meteo(ctx, search: str):
         meteo = f"La température à {city} est de {result} C°"
         return meteo
         
-    def KelvinToCelcius(kTemp):
-        cTemp = kTemp - 273.15
-        return cTemp
-
     meteo = FindMeteo()
     await ctx.send(meteo)
 
@@ -43,27 +57,24 @@ async def MeteoHour(ctx, search: str):
     city = search 
     apiKey = 'a440a00c0ff4abe85ed3319d7e208d73'
     url = f"http://api.openweathermap.org/data/2.5/forecast?q={city}&appid={apiKey}"
-    dateChoice = '2021-07-03'    
+    dateChoice = '2021-07-02'    
     r = requests.get(url).json()
 
     list_ = r["list"] #List contient un tableau avec chaque tranche 3H
-    
-    dataList = [] #DataList continet un tableau avec chaque tranche 3h de la date choisie
+    dataList = []
 
     for x in range(len(list_)):
-        if list_[x]["dt_txt"][0:10] == dateChoice and list_[x]["dt_txt"][11:16] != '00:00' and list_[x]["dt_txt"][11:16] != '03:00':
-            dataList.append({"heure" : list_[x]["dt_txt"][11:16], "value" : round(KelvinToCelcius( list_[x]["main"]["temp"]),2)}) 
-
-    await ctx.send(f'Meteo pour le {dateChoice} à {city}, \n')
-    for y in range(len(dataList)):
-        await ctx.send(f'{dataList[y]["heure"]} Temperature = {dataList[y]["value"]} C°')
-
-
+        if list_[x]["dt_txt"][0:10] == dateChoice:
+            parsed = datetime.strptime(list_[x]["dt_txt"], "%Y-%m-%d %H:%M:%S")
+            parsed = parsed - timedelta(hours=1, minutes=0)
+            timeExtract = "{:02d}:{:02d}".format(parsed.hour, parsed.minute)
+            print(timeExtract)
+            resp = f'{timeExtract}, {round(KelvinToCelcius(list_[x]["main"]["temp"]),2)} C°'
+            await ctx.send(resp) 
 
 def KelvinToCelcius(kTemp):
         cTemp = kTemp - 273.15
         return cTemp
 
 
-bot.run('ODU5ODgyOTQ0MzgxMDU5MTQy.YNzKZQ.2osL7UGcaYoT6nKln4tdS1mJhYk')
-#print(json.dumps(list_, indent=4, sort_keys=True))
+bot.run('ODU5ODgyOTQ0MzgxMDU5MTQy.YNzKZQ.8h2CYVX9Up6G0zC4tuAuqiJKZ9A')
